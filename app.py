b1743d661c324e3a19f41f7d85e8d2b4e7710fd8
Ajaay Krishna <pajaaykrishna@gmail.com>
2026-07-10 12:43:41 +0530

    Fourth Commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "World from AWS!"
+    return "Hello World from AWS!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
